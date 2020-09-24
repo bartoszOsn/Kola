9a8dd9696d43c9f77817e5bf489e9c74784e3f89
bartoszOsn <osinskibartosz24@gmail.com>
2020-09-24 15:25:53 +0200

    Added Base Comic book

---
+++ Kola/Model/ComicBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Kola.Model
+{
+    abstract class ComicBook
+    {
+        public string Name { get; set; }
+        public abstract int PageNumber { get; set; }
+        public abstract int PageCount { get; }
+        public abstract ImageSource Page { get; }
+
+        public void NextPage()
+        {
+            PageNumber++;
+        }
+        public void PreviousPage()
+        {
+            PageNumber--;
+        }
+    }
+}
