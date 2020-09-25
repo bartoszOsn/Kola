3512b2f3ca45ac6d5150fb8357d00790c840ccb3
bartoszOsn <osinskibartosz24@gmail.com>
2020-09-25 10:45:26 +0200

    Implemented Tabs except events

--- Kola/Controls/Tabs.xaml.cs
+++ Kola/Controls/Tabs.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,27 @@ using System.Windows.Shapes;
 
 namespace Kola.Controls
 {
+    public struct StringEntry
+    {
+        public string Text { get; set; }
+        public int Index { get; set; }
+        public bool Selected { get; set; }
+    }
+    public class StringArrayConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            int selectedIndex = (int)values[1];
+            return ((IEnumerable<string>)(values[0])).Select((t, i) => new StringEntry() { Text = t, Index = i, Selected = i == selectedIndex });
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     /// <summary>
     /// Interaction logic for Tabs.xaml
     /// </summary>
@@ -31,6 +53,19 @@ namespace Kola.Controls
         // Using a DependencyProperty as the backing store for TabNames.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TabNamesProperty =
             DependencyProperty.Register("TabNames", typeof(IEnumerable<string>), typeof(Tabs), new PropertyMetadata(new string[] { "Hellblazer #341", "IZombie #1"}));
+
+
+
+        public int SelectedIndex
+        {
+            get { return (int)GetValue(SelectedIndexProperty); }
+            set { SetValue(SelectedIndexProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedIndex.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedIndexProperty =
+            DependencyProperty.Register("SelectedIndex", typeof(int), typeof(Tabs), new PropertyMetadata(1));
+
 
 
         public Tabs()
