1d590dd28965d59b397699c1b2aed067a99be650
bartoszOsn <osinskibartosz24@gmail.com>
2020-10-25 14:14:30 +0100

    Created extension that blends two solid colors and set as background in combobox

---
+++ Kola/MarkupExtensions/BlendColorExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Kola.MarkupExtensions
+{
+    class BlendColorExtension : MarkupExtension
+    {
+        public SolidColorBrush A { get; set; }
+        public SolidColorBrush B { get; set; }
+        public float Mix { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            Color A = this.A.Color;
+            Color B = this.B.Color;
+            Color Result = new Color();
+            Result.ScR = A.ScR * Mix + B.ScR * (1 - Mix);
+            Result.ScG = A.ScG * Mix + B.ScG * (1 - Mix);
+            Result.ScB = A.ScB * Mix + B.ScB * (1 - Mix);
+            Result.ScA = A.ScA * Mix + B.ScA * (1 - Mix);
+
+            return Result;
+        }
+    }
+}
