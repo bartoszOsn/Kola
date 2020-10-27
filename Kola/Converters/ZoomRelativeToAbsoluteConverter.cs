761d9f62d9ea2117f841491c9cc4af228567c0d5
bartoszOsn <osinskibartosz24@gmail.com>
2020-10-27 10:03:26 +0100

    Implemented ZoomRelativeToAbsoluteConverter

---
+++ Kola/Converters/ZoomRelativeToAbsoluteConverter.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Kola.Converters
+{
+    enum ZoomProperty
+    {
+        OffsetX,
+        OffsetY
+    }
+
+    /// <summary>
+    /// Converts relative values from model to useful absolute values in pixels.
+    /// Bindings should be assigned in this order:
+    /// <list type="number">
+    ///     <item>property from <c>Zoom</c> model</item>
+    ///     <item>Image control</item>
+    ///     <item>ZoomProperty describing which property we want to get.</item>
+    /// </list>
+    /// </summary>
+    class ZoomRelativeToAbsoluteConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// Converts given values to expected property
+        /// </summary>
+        /// <param name="values">
+        ///     It should be following values:
+        ///     <list type="number">
+        ///         <item>property from <c>Zoom</c> model</item>
+        ///         <item>Image control</item>
+        ///         <item>ZoomProperty describing which property we want to get.</item>
+        ///     </list>
+        /// </param>
+        /// <param name="targetType">Not used</param>
+        /// <param name="parameter">Not used</param>
+        /// <param name="culture">Not used</param>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values.Length < 3)
+            {
+                return null;
+            }
+            double prop = (double)values[0];
+            Image image = values[1] as Image;
+            ZoomProperty zoomProperty = (ZoomProperty)values[2];
+
+            switch(zoomProperty)
+            {
+                case ZoomProperty.OffsetX:
+                    return GetOffsetX(prop, image);
+                case ZoomProperty.OffsetY:
+                    return GetOffsetY(prop, image);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Not supported.
+        /// </summary>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns real offset in pixels.
+        /// </summary>
+        private double GetOffsetX(double offsetX, Image image)
+        {
+            double width = GetRealWidth(image);
+            double viewportWidth = image.ActualWidth;
+            if(width <= viewportWidth)
+            {
+                return 0;
+            }
+            return (width - viewportWidth) * offsetX;
+        }
+
+        /// <summary>
+        /// Returns real offset in pixels.
+        /// </summary>
+        private double GetOffsetY(double offsetY, Image image)
+        {
+            double width = GetRealWidth(image);
+            double viewportWidth = image.ActualWidth;
+            if (width <= viewportWidth)
+            {
+                return 0;
+            }
+            return (width - viewportWidth) * offsetY;
+        }
+
+        /// <summary>
+        /// Returns real width of image, after applying ScaleTransform.
+        /// </summary>
+        private double GetRealWidth(Image img)
+        {
+            TransformGroup group = img.RenderTransform as TransformGroup;
+            return ((ScaleTransform)group.Children.First(t => t is ScaleTransform)).ScaleX * img.ActualWidth;
+        }
+
+
+        /// <summary>
+        /// Returns real height of image, after applying ScaleTransform.
+        /// </summary>
+        private double GetRealHeight(Image img)
+        {
+            TransformGroup group = img.RenderTransform as TransformGroup;
+            return ((ScaleTransform)group.Children.First(t => t is ScaleTransform)).ScaleY * img.ActualHeight;
+        }
+    }
+}
---
+++ Kola/Model/Zoom.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kola.Model
+{
+    class Zoom : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Level of zoom, value <c>1</c> means no zoom at all.
+        /// Value of this property must be greater or equal to 1.
+        /// </summary>
+        public double ZoomLevel
+        {
+            get => zoomLevel;
+            set
+            {
+                zoomLevel = value;
+                Changed(nameof(ZoomLevel));
+            }
+        }
+        /// <summary>
+        /// Horizontal offset of zoomed image.
+        /// It is relative to ZoomLevel and image size, meaning that <c>OffsetX = 1</c> means maximum offset, when right edge of image overlaps with right edge of viewport.
+        /// </summary>
+        public double OffsetX
+        {
+            get => offsetX;
+            set
+            {
+                offsetX = value;
+                Changed(nameof(OffsetX));
+            }
+        }
+        /// <summary>
+        /// Vertical offset of zoomed image.
+        /// It is relative to ZoomLevel and image size, meaning that <c>OffsetY = 1</c> means maximum offset, when bottom edge of image overlaps with bottom edge of viewport.
+        /// </summary>
+        public double OffsetY
+        {
+            get => offsetY;
+            set
+            {
+                offsetY = value;
+                Changed(nameof(OffsetY));
+            }
+        }
+
+        private double zoomLevel = 1;
+        private double offsetX = 0;
+        private double offsetY = 0;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Changed(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
